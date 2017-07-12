639efcdba2ba4419ee12cc91c53972b66f642a29
심명훈 <julsnutini@naver.com>
2017-07-12 16:57:21 +0900

    건국대 체육관 -> 충북대 폴리라인

--- app/src/main/java/com/example/mh/googlemapapidemo/MapsActivity.java
+++ app/src/main/java/com/example/mh/googlemapapidemo/MapsActivity.java
@@ -2,16 +2,19 @@ package com.example.mh.googlemapapidemo;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -41,9 +44,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        // 충북대 마커
         LatLng CBNU = new LatLng(36.628305, 127.458241);
-        mMap.addMarker(new MarkerOptions().position(CBNU).title("충북대"));
+        mMap.addMarker(new MarkerOptions().position(CBNU).title("충북대")
+            .position(CBNU)
+            .title("ddddd")
+            .icon(BitmapDescriptorFactory.fromResource(R.drawable.school16x16))
+        );
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CBNU, 13));
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -55,6 +62,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+
+        // 경기장 마커
+        LatLng Stardium = new LatLng(36.949233, 127.909649);
+        mMap.addMarker(new MarkerOptions().position(Stardium).title("건국대학교체육관")
+                .position(Stardium)
+                .title("ddddd")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.stardium16x16))
+        );
+
+        mMap.addPolyline(new PolylineOptions().add(
+                Stardium,
+                new LatLng(36.628305, 127.458241), //건국대 체육관에서 충북대까지 선 긋기
+                CBNU
+                )
+                .width(10)
+                .color(Color.RED)
+
+        );
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Stardium, 13));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
         mMap.setMyLocationEnabled(true);
     }
 }
